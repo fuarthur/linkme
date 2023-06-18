e5de9a66bcde6ce40ab927c109b6d03b4597d1af
michael <perfeipu@qq.com>
2023-06-18 13:40:50 +0800

    feat: profile page

--- app/src/main/java/com/ams/linkme/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/ams/linkme/ui/activity/ProfileActivity.kt
@@ -1,91 +1,92 @@
 package com.ams.linkme.ui.activity
 
-import android.R
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.ams.linkme.R
 import com.ams.linkme.ui.viewmodel.ProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ProfileActivity : AppCompatActivity() {
-    private var profileViewModel: ProfileViewModel? = null
-    private var interestsListView: ListView? = null
-    private var nameEditText: EditText? = null
-    private var ageEditText: EditText? = null
-    private var genderEditText: EditText? = null
-    private var submitButton: Button? = null
+    private lateinit var profileButton: Button
+    private lateinit var radioButtonMale: RadioButton
+    private lateinit var imageViewPerson: ImageView
+    private lateinit var radioButtonFemale: RadioButton
+    private lateinit var radioGroupGender: RadioGroup
+    private lateinit var editTextPhone: EditText
+    private lateinit var submitButton: Button
+    private lateinit var sport: CheckBox
+    private lateinit var food: CheckBox
+    private lateinit var dating: CheckBox
+    private lateinit var games: CheckBox
+    private lateinit var music: CheckBox
+    private lateinit var movie: CheckBox
+    private lateinit var anime: CheckBox
+    private lateinit var other: CheckBox
+    private lateinit var profileViewModel: ProfileViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        interestsListView = findViewById<ListView>(R.id.interestsListView)
-        nameEditText = findViewById<EditText>(R.id.nameEditText)
-        ageEditText = findViewById<EditText>(R.id.ageEditText)
-        genderEditText = findViewById<EditText>(R.id.genderEditText)
-        submitButton = findViewById<Button>(R.id.submitButton)
 
-        // 设置按钮勾选的兴趣供用户选择
-        val interests = arrayOf("Interest 1", "Interest 2", "Interest 3")
-        val adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_multiple_choice, interests
-        )
-        interestsListView.setAdapter(adapter)
-        interestsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE)
+        // Initialize all views
+        profileButton = findViewById(R.id.profile_button)
+        radioButtonMale = findViewById(R.id.radio_button_male)
+        imageViewPerson = findViewById(R.id.imageView)
+        radioButtonFemale = findViewById(R.id.radio_button_female)
+        radioGroupGender = findViewById(R.id.radio_group_gender)
+        editTextPhone = findViewById(R.id.editTextPhone)
+        submitButton = findViewById(R.id.button)
+        sport = findViewById(R.id.Sports)
+        food = findViewById(R.id.Food)
+        dating = findViewById(R.id.Dating)
+        games = findViewById(R.id.Games)
+        music = findViewById(R.id.Music)
+        movie = findViewById(R.id.Movie)
+        anime = findViewById(R.id.Anime)
+        other = findViewById(R.id.Other)
+        profileViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        // 监听按钮勾选的兴趣
-        interestsListView.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
-            val selectedItems = interestsListView.getCheckedItemPositions()
-            val selectedInterests: MutableList<String> = ArrayList()
-            for (i in 0 until selectedItems.size()) {
-                if (selectedItems.valueAt(i)) {
-                    selectedInterests.add(interests[selectedItems.keyAt(i)])
-                }
+        submitButton.setOnClickListener {
+            // Get selected radio button from radioGroup
+            val gender = when (radioGroupGender.checkedRadioButtonId) {
+                R.id.radio_button_male -> "Male"
+                R.id.radio_button_female -> "Female"
+                else -> "Not Specified"
             }
-            profileViewModel!!.interests.setValue(selectedInterests)
-        })
-
-        // 监听输入框的文本变化
-        nameEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                profileViewModel!!.name.setValue(s.toString())
+            // Get inputs from edit texts
+            val phone = editTextPhone.text.toString().trim()
+            val interests = ArrayList<String>()
+            if (sport.isChecked) {
+                interests.add("Sport")
             }
-
-            override fun afterTextChanged(s: Editable) {}
-        })
-        ageEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                val age = Integer.valueOf(s.toString())
-                profileViewModel!!.age.setValue(age)
+            if (food.isChecked) {
+                interests.add("Food")
             }
-
-            override fun afterTextChanged(s: Editable) {}
-        })
-        genderEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                profileViewModel!!.gender.setValue(s.toString())
+            if (dating.isChecked) {
+                interests.add("Dating")
             }
-
-            override fun afterTextChanged(s: Editable) {}
-        })
-
-        // 监听提交按钮点击事件
-        submitButton.setOnClickListener(object : OnClickListener() {
-            fun onClick(v: View?) {
-                profileViewModel!!.submit()
+            if (games.isChecked) {
+                interests.add("Games")
             }
-        })
+            if (music.isChecked) {
+                interests.add("Music")
+            }
+            if (anime.isChecked) {
+                interests.add("Anime")
+            }
+            if (movie.isChecked) {
+                interests.add("Movie")
+            }
+            if (other.isChecked) {
+                interests.add("Other")
+            }
+            val auth = FirebaseAuth.getInstance()
+            val user = auth.currentUser
+            val uid = user!!.uid // The user's ID, unique to the Firebase project.
+            profileViewModel.submit(uid, gender, phone, interests)
+        }
     }
 }
 
--- app/src/main/java/com/ams/linkme/ui/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/ams/linkme/ui/viewmodel/ProfileViewModel.kt
@@ -1,29 +1,12 @@
 package com.ams.linkme.ui.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
 class ProfileViewModel : ViewModel() {
-    val interests = MutableLiveData<List<String>>()
-    val name = MutableLiveData<String>()
-    val age = MutableLiveData<Int>()
-    val gender = MutableLiveData<String>()
 
-    fun submit() {
-        val selectedInterests = interests.value!!
-        val enteredName = name.value
-        val enteredAge = age.value
-        val enteredGender = gender.value
-
-        // 将收集到的参数提交信息
-        // 在这里进行提交逻辑的处理，例如发送网络请求或保存到数据库等
-
-        // 打印提交的参数
-        println("Selected Interests: $selectedInterests")
-        println("Name: $enteredName")
-        println("Age: $enteredAge")
-        println("Gender: $enteredGender")
+    fun submit(uid: String, gender: String, phone: String, interests: ArrayList<String>) {
+        // TODO
     }
 }
 
