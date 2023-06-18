aebcf9d034031a3f0a2daf06cd5e02be32c332fd
ARtcgb <71631622+ARtcgb@users.noreply.github.com>
2023-06-18 17:39:47 +0800

    feat: main query

--- app/src/main/java/com/ams/linkme/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ams/linkme/ui/activity/MainActivity.kt
@@ -30,19 +30,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         linkButton = findViewById(R.id.linkbutton)
         interestText = findViewById(R.id.searchEditText)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         linkButton.setOnClickListener {
-            mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
-            userList = mainViewModel.link(interestText.text.toString().trim())
-            Log.d("UserList", userList.toString())
-            // handleUserList(userList)
+            mainViewModel.link(interestText.text.toString().trim(), object : MainViewModel.LinkCallback {
+                override fun onLinkResult(userList: List<User>) {
+                    handleUserList(userList)
+                    // Log.d("debug-annotation", userList.toString())
+                }
+            })
             bottomNavigate()
         }
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
         return true
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -66,14 +71,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleUserList(userList: List<User>) {
-        // Access the userList and perform desired operations
-        // For example, update the UI with the userList data
+        // 获取RecyclerView视图组件
         val recyclerView: RecyclerView = findViewById(R.id.userRecyclerView)
+
+        // 创建并设置适配器
         val adapter = UserAdapter(userList)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
     }
-    fun bottomNavigate() {
+    private fun bottomNavigate() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
--- app/src/main/java/com/ams/linkme/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ams/linkme/ui/viewmodel/MainViewModel.kt
@@ -1,30 +1,47 @@
 package com.ams.linkme.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.ams.linkme.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.tasks.await
 import java.util.*
 
 class MainViewModel : ViewModel() {
     private lateinit var firestore: FirebaseFirestore
 
-    fun link(interest: String): List<User> {
+    interface LinkCallback {
+        fun onLinkResult(userList: List<User>)
+    }
+
+    fun link(interest: String, callback: LinkCallback) {
         firestore = Firebase.firestore
         val userList = mutableListOf<User>()
+
+        // 获取登录用户的UID
+        val auth = FirebaseAuth.getInstance()
+        val currentUserUid = auth.currentUser?.uid
+
         firestore.collection("Users")
-            .whereArrayContains("interest", interest.lowercase(Locale.getDefault()))
+            .whereArrayContains("interests", interest)
+            .whereNotEqualTo(FieldPath.documentId(), currentUserUid)
             .limit(3)
             .get()
             .addOnSuccessListener { querySnapshot ->
-                // Handle query results
                 for (document in querySnapshot) {
-                    val user = document.toObject(User::class.java)
+                    val username = document.getString("username")
+                    val email = document.getString("email")
+                    val gender = document.getString("gender")
+                    val phone = document.getString("phone")
+                    val interests = document.get("interests") as? ArrayList<String>
+                    val user = User(username, email, gender, phone, interests)
                     userList.add(user)
                 }
+                callback.onLinkResult(userList)
             }
-        return userList
     }
 }
+
